ac9b2b56be14c45f8ed7347a502ab9b11ae5747d
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2020-11-13 20:22:55 +0700

    build: Add snowpack-plugin-rollup-bundle

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,6 +1,6 @@
 const { BROWSER } = process.env
 module.exports = {
-    exclude: [".scripts/**", "vendor/install.*js"],
+    exclude: [".scripts/**", "vendor/install.*js", "**/*.d.ts"],
     devOptions: {
         open: BROWSER ? ".scripts/browser.sh" : "none",
         secure: true,
@@ -10,7 +10,17 @@ module.exports = {
     plugins: [
         ["@snowpack/plugin-optimize", {
             target: ["chrome83"], // target WebXR DOM Overlays
-            preloadModules: true
+            preloadModules: false // can cause duplicated <script>
+        }],
+        ["snowpack-plugin-rollup-bundle", {
+            entrypoints: [
+                "build/src/main.js", "build/res/main.css",
+                "build/vendor/ammojs/index.js",
+                "build/vendor/ARCore/arcore_devicelist.csv",
+                "build/web_modules/device-detector-js.js", "build/web_modules/@babylonjs/core.js"
+            ],
+            preserveSourceFiles: true,
+            emitHtmlFiles: true
         }],
         ["@snowpack/plugin-sass", { native: true }],
         "@snowpack/plugin-dotenv",
